f622e896390c91f9d67b6d1f46d133d81a03f32f
Samia Beggar <samiabeggar20@gmail.com>
2023-02-04 17:29:06 +0100

    header.php sans php

---
+++ header.php
@@ -0,0 +1,5 @@
+<div id="login_container">
+<a href="index.php">
+         <img src="images/logo_gbaf.png" alt="logo GBAF" style="width:130px;height:130px;"/>
+</a>
+         <h1> Le Groupement Banque Assurance Français </h1>
