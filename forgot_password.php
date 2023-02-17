af1ff9c06d3e5c94d5675f3fa8bf531b49b52ebb
Samia Beggar <samiabeggar20@gmail.com>
2023-02-17 03:16:59 +0100

    modification formulaire

--- forgot_password.php
+++ forgot_password.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include 'dbb_connexion.php';
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -14,17 +19,26 @@
   <body>
   <?php include_once 'header.php'; ?>
         <!--formulaire récupération mot de passe-->
-        <form method="post" action="forgot_password.php">
+        <form method="post" action="forgot_password_traitement.php">
           <fieldset>
           <legend>
           <i class="fa fa-university" aria-hidden="true" style="color : red;"></i>
           Réinitialiser votre mot de passe :
           </legend>
             <input type="text" name="username" placeholder="Identifiant" required />  
+            <input list="question" name="question"placeholder="Choissisez votre question secrète" required >
+            <datalist id="question" >
+               <option value="Quel est le nom de votre ville natale ?">
+               <option value="Quel est le nom de votre meilleur/e ami/e?">
+               <option value="Quel est le nom de famille de votre professeur d’enfance préféré ?">
+               <option value="Dans quelle ville se sont rencontrés vos parents? ">
+             </datalist>
+            <input type="text" name="reponse" placeholder=" La réponse " required  />
             <input type="submit" value="Envoyer" />
           </fieldset>
         </form>
   </body>
+  
        <?php include_once 'footer.php'; ?>
 
 </html>
