24e46329d701700b863f2aac153b3d3f67cb3820
Samia Beggar <samiabeggar20@gmail.com>
2023-02-20 02:35:33 +0100

    header logged

---
+++ header_log.php
@@ -0,0 +1,11 @@
+
+<header>
+        <a href="index.php" >
+        <img src="images/logo_gbaf.png" alt="Logo GBAF miniature" style= "margin-left: 0; width:45px;height:45px "/>
+        </a>
+        <p>Bienvenue <?php echo  $_SESSION['firstname'] , $_SESSION['name'] ; ?></p>
+
+        <a href="account.php">Mon Compte</a>    
+            
+        <a href="deconnexion.php">Deconnexion</a>
+    </header>
