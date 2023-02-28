a60d5f8ad3738c0aa7f19cb24865587d8a49e9b6
Samia Beggar <samiabeggar20@gmail.com>
2023-02-28 18:47:16 +0100

    correction

--- actor.php
+++ actor.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 
 <html>
@@ -18,7 +15,8 @@ session_start();
 <?php include 'dbb_connexion.php'; ?>
 <?php include 'header_log.php' ; ?>
 
-<div class="actor_details">
+<section id="actor_details">
+
 <?php 
 
   $id = $_GET['id'];
@@ -32,16 +30,19 @@ session_start();
 }
 ?>
 <!-- afficher les données de l'acteur --> 
-<img src="<?php echo $actor['logo']; ?>" alt= "Logo partenaires" ?>
+
+<img src="<?php echo $actor['logo']; ?>" alt= "Logo partenaire" ?>
+
 <h2><?php echo $actor['acteur']; ?></h2>
 <a href="#">Visiter le site de <?php echo $actor['acteur'];?></a><br>
 <p><?php echo  nl2br ($actor['description'] ) ;?></p>
+
 </section>
 
 
-<section>
 
-<div class="comments">
+<section id="comments_container">
+<div class= "comments_option">
 <?php
 // nombre des commentaires
  $id = $_GET['id'];
@@ -59,6 +60,7 @@ session_start();
     ?>
 
    <!--ajouter un commentaire-->
+
     <?php
     $id_user= $_SESSION['id_user'];
     $sqlQuery='SELECT * FROM post WHERE id_user = :id_user AND id_acteur = :id_acteur';
@@ -83,12 +85,18 @@ session_start();
         <a href="actor_comments_new.php?id=<?php echo $actor['id_acteur']; ?>">
         <p> Nouveau commentaire </p>
         </a>
-        <?php
-    }
-    ?>
-    
+        </div>
 
-    <div class="votes">
+      <?php
+      }
+      ?>
+
+
+
+
+
+
+   
      <!-- nombre des likes , dislikes pour l'acteur -->
       <!--vote=1 : LIKE-->
      <?php
@@ -103,27 +111,69 @@ session_start();
      $dislikes->execute(array($_GET['id']));
      $nbDislikes= $dislikes-> rowCount();
      ?>
-     
+    
+
+
+    <?php
+$sqlQuery= 'SELECT * FROM vote WHERE id_acteur= :id_acteur AND id_user=:id_user ';
+$sql=$db->prepare($sqlQuery);
+$sql->execute(array(':id_acteur'=> $id ,':id_user'=> $id_user ));
+$data=$sql->fetch();
+
+?>
+
+
+
+    
        <!-- formulaire de vote  -->
-      <form method="post" action= "votes_traitement.php" >
+       <div class="votes">
+<form method="post" action= "actor.php" >
 
-       <span class="vote_count"><?php echo $nbLikes - $nbDislikes; ?></span> 
+<span class="vote_count"><?php echo $nbLikes ; ?></span> 
 
-        <input type=hidden name="id_acteur" value ="<?php echo $id; ?>">
-        <input type="hidden" name="id_user" value="<?php echo $id_user; ?>">
-        <button class="like" type="submit" name="vote" value="1">
-          <i id="id_like" class="fa fa-thumbs-up" style="font-size:22px"></i>
-        </button>
+ <input type=hidden name="id_acteur" value ="<?php echo $id; ?>">
+ <input type="hidden" name="id_user" value="<?php echo $id_user; ?>">
+ <button class="like" type="submit" name="vote" value="1" <?php if($data) echo "disabled"; ?> >
+   <i id="id_like" class="fa fa-thumbs-up" style="font-size:22px"></i>
+ </button>
+ <span class="vote_count"><?php echo $nbDislikes ; ?></span> 
+ <button class="dislike" type="submit" name="vote" value="2" <?php if($data) echo "disabled"; ?> >
+   <i id="id_dislike" class="fa fa-thumbs-down" style="font-size:22px"></i>
+ </button> 
+</form>
+    </div>
+<?php
+if  (!$data)
+{
+  $id = $_GET['id'];
+}
 
-        <button class="dislike" type="submit" name="vote" value="2">
-          <i id="id_dislike" class="fa fa-thumbs-down" style="font-size:22px"></i>
-        </button> 
-      </form>
+
+if (isset($_POST['vote'])) {
+  $vote = $_POST['vote'];
+  $id_user= $_SESSION['id_user'];
+  $id = $_GET['id'];
+//$id = $_POST['id_acteur'];
+//$id = $_GET['id'];
+//$id_user= $_SESSION['id_user'];
+//$vote = $_POST['vote'];
+$sqlQuery= 'INSERT INTO vote ( id_user, id_acteur, vote) VALUES (:id_user, :id_acteur, :vote)' ; 
+$sql=$db->prepare($sqlQuery);
+$sql->execute(array(
+   ':id_user' => $id_user,
+   ':id_acteur' => $id,
+    ':vote' => $vote
+));
+}
+?>
+
+
+
 
 
 
       <!-- Afficher les commentaires laissés par les autres users-->
-      <div class="other_comments">
+      
       <?php
 
         $sqlQuery=('SELECT * FROM post INNER JOIN account ON post.id_user = account.id_user WHERE id_acteur = :id_acteur ORDER BY date_add DESC LIMIT 3');
@@ -132,16 +182,15 @@ session_start();
         while ($data_comment = $query -> fetch())
         {
         ?>
-         
+         <div class="other_comments">
          <p> <?php echo($data_comment['prenom']); ?> </p>
          <p> <?php echo $data_comment['date_add']; ?> </p>
          <p> <?php echo nl2br($data_comment['post']); ?> </p>
-      
+         </div>
         <?php
         }
         ?>
-
-</section>
+      </section>
 
 
 <?php include_once 'footer.php'; ?>
