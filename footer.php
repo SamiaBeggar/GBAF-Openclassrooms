c7a45a1f928b0e5114253bf40351b155d9e1ebca
Samia Beggar <samiabeggar20@gmail.com>
2023-03-07 17:11:35 +0100

    avancement

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
-<footer class="w3-container w3-center">
-<div id="mydiv">
-    <a href="#">Mentions légales</a>
-     |
-    <a href="#">Contact</a>
-</div>
 
-</footer>
+<footer>
+    <a href="#">Mentions légales</a>
+    |
+    <a href="#">Contact</a>
+ </footer>
