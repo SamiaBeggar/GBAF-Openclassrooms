b66509f8d90aca07cae8adb59cce97621272bea6
Samia Beggar <samiabeggar20@gmail.com>
2023-03-07 17:12:18 +0100

    avancement

--- forgot_password.php
+++ forgot_password.php
@@ -4,11 +4,12 @@ include 'dbb_connexion.php';
 ?>
 
 <!DOCTYPE html>
+<html lang="fr">
 
-<html>
 
   <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>GBAF-Mot de passe oublié</title>
         <link href="style.css" rel="stylesheet">
         <link rel="icon" type="image/png" sizes="16x16"  href="images/favicon.ico.png">
@@ -44,16 +45,16 @@ include 'dbb_connexion.php';
           <i class="fa fa-university" aria-hidden="true" style="color : red;"></i>
           Réinitialiser votre mot de passe :
           </legend>
-            <input type="text" name="username" placeholder="Identifiant" required />  
-            <input list="question" name="question"placeholder="Choissisez votre question secrète" required >
+            <input type="text" name="username" placeholder="Identifiant" required>  
+            <input list="question" name="question" placeholder="Choissisez votre question secrète" required>
             <datalist id="question" >
                <option value="Quel est le nom de votre ville natale ?">
                <option value="Quel est le nom de votre meilleur/e ami/e?">
                <option value="Quel est le nom de famille de votre professeur d’enfance préféré ?">
                <option value="Dans quelle ville se sont rencontrés vos parents? ">
              </datalist>
-            <input type="text" name="reponse" placeholder=" La réponse " required  />
-            <input type="submit" value="Envoyer" />
+            <input type="text" name="reponse" placeholder=" La réponse " required>
+            <input type="submit" value="Envoyer">
           </fieldset>
         </form>
   </body>
