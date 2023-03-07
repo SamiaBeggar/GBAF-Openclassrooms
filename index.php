16084e36a1913ce7e92820993780f90731edb032
Samia Beggar <samiabeggar20@gmail.com>
2023-03-07 17:23:33 +0100

    avancement

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ session_start();
 ?>
 
 <!DOCTYPE html>
+<html lang="fr">
 
-<html>
 
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>GBAF-Extranet</title>
         <link href="style.css" rel="stylesheet">
         <link rel="icon" type="image/png" sizes="16x16"  href="images/favicon.ico.png">
@@ -17,6 +18,7 @@ session_start();
 
     <body>
     <?php include_once 'header.php'; ?>
+    <?php include 'dbb_connexion.php'; ?>
     <?php
           if(isset($_GET['login_err']))
             {
@@ -29,7 +31,7 @@ session_start();
                     <p><strong>Erreur</strong> Mot de passe incorrect !</p>
                   <?php
                   break;
-                  case 'username' :
+                  case 'already' :
                   ?>
                     <p><strong>Erreur</strong> Compte introuvable !</p>
                   <?php
@@ -38,24 +40,26 @@ session_start();
             }
             ?>
         <!--formulaire de connexion-->
-        <form method="post" action="index.php">
+        <form method="post" action="login.php">
           <fieldset>
           <legend>
           <i class="fa fa-university fa-xs" aria-hidden="true" style="color : red;"></i>
           Connexion :
           </legend>
-          <input type="text" name="username" placeholder="Identifiant" /> <br>
-          <input type="password" name="password" placeholder="Mot de passe"  /> <br>
-          <input type="submit" value="Envoyer" /><br><br>
+          <input type="text" name="username" placeholder="Identifiant" > <br>
+          <input type="password" name="password" placeholder="Mot de passe"  > <br>
+          <input type="submit" value="Envoyer" ><br><br>
           <p>
-            <a href="forgot_password.php">Mot de passe oublié ?</a> </br>
+            <a href="forgot_password.php">Mot de passe oublié ?</a> <br>
             <a href="create_account.php">Pas encore de compte ? Inscrivez-vous !</a>
           </p>
           </fieldset>
         </form>
 
-        
-    </body>
+        <?php include_once 'footer.php'; ?>
+
+
+     </body>
     
-    <?php include_once 'footer.php'; ?>
+    
 </html>
