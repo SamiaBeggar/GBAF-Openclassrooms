8bbd2eae4f8604f6b19c1406e4fcd55508a61588
Samia Beggar <samiabeggar20@gmail.com>
2023-03-07 17:21:21 +0100

    avancement

--- header_log.php
+++ header_log.php
@@ -1,11 +1,17 @@
-
+<?php
+session_start();
+?>
 <header>
-        <a href="index.php" >
-        <img src="images/logo_gbaf.png" alt="Logo GBAF miniature" style= "margin-left: 0; width:45px;height:45px "/>
+        
+        <a href="home.php" class="logomin">
+        <img src="images/logo_gbaf.png" alt="Logo GBAF miniature" style= "margin-left: 0; width:65px;height:65px ">
         </a>
-        <p>Bienvenue <?php echo  $_SESSION['firstname'] , $_SESSION['name'] ; ?></p>
 
-        <a href="account.php">Mon Compte</a>    
+        <nav class="rightheader">
+           <p>Bienvenue <?php echo  $_SESSION['firstname'] ; ?> <?php echo  $_SESSION['name'] ; ?></p>
+
+           <a href="account.php">Mon Compte</a>    
             
-        <a href="deconnexion.php">Deconnexion</a>
+           <a href="deconnexion.php">Deconnexion</a>
+        </nav>
     </header>
