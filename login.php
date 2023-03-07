1a038285c79767776214900bd2565b3afd1de525
Samia Beggar <samiabeggar20@gmail.com>
2023-03-07 17:24:31 +0100

    avancement

--- login.php
+++ login.php
@@ -7,29 +7,35 @@ if(!empty($_POST['username']) && !empty($_POST['password']))
     $username = htmlspecialchars($_POST['username']); 
     $password = htmlspecialchars($_POST['password']);
 
-    $check = $db->prepare("SELECT *  FROM account WHERE username=?");
+    $check = $db->prepare ("SELECT *  FROM account WHERE username=?");
     $check->execute(array($username));
     $data_login = $check->fetch();
     $row = $check->rowCount();
     // Si > 0 , user existe
      if($row > 0)
-    {
+    { 
         //vérifier le mdp 
-        if(password_verify($password, $data['password']))
-        {
+        if(password_verify($password, $data_login['password']))
 
+        //if ($password==$data_login['password'])
+
+        {
+        
             $_SESSION['id_user'] = $data_login['id_user'];
-            $_SESSION['firstname'] = $data_login['firstname'];
-            $_SESSION['name'] = $data_login['name'];
+            $_SESSION['firstname'] = $data_login['prenom'];
+            $_SESSION['name'] = $data_login['nom'];
             $_SESSION['username'] = $data_login['username'];
             $_SESSION['password'] = $data_login['password'];
-            $_SESSION['secret_question'] = $data_login['secret_question'];
-            $_SESSION['answer'] = $data_login['answer'];
-            header('Location:home.php');
+            $_SESSION['secret_question'] = $data_login['question'];
+            $_SESSION['answer'] = $data_login['reponse'];
+            header ('Location: home.php');
 
-        }else header('Location: index.php');
+        }else{
 
-    }else header('Location: index.php?login_err=password');
-} else header ('location : index.php?login_err=username' );
+         header('Location: index.php?login_err=password');
+        }
+
+    }else header('Location: index.php?login_err=already');
+} else header ('location : index.php' );
 
 ?>
